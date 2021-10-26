aa56e8b98d5dd1b5046d8dcfd76b870822f0334c
Ivan <ivennsfroese@gmail.com>
2021-10-26 13:46:50 -0500

    Added Main Gui class

---
+++ theGameOfLife/src/theGameOfLife/MainGui.java
@@ -0,0 +1,5 @@
+package theGameOfLife;
+
+public class MainGui {
+	//This will house the main frame, the grid, the Start, Stop, etc buttons, the ComboBox for pre-made patterns
+}
--- theGameOfLife/src/theGameOfLife/Rules.java
+++ theGameOfLife/src/theGameOfLife/Rules.java
@@ -1,5 +1,6 @@
 package theGameOfLife;
 
-public class Rules {
+public class Rules {	
+	
 
 }
