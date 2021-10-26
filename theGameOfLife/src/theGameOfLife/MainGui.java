715ff4f1235f54f38184090195d3cb4d831a9cd6
Ivan <ivennsfroese@gmail.com>
2021-10-26 14:15:00 -0500

    Added Combo Box

--- theGameOfLife/src/theGameOfLife/MainGui.java
+++ theGameOfLife/src/theGameOfLife/MainGui.java
@@ -1,24 +1,14 @@
 package theGameOfLife;
-
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.color.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
-
-
-import javax.swing.*;
-
+import java.awt.*;
 public class MainGui {
+	//This will house the main frame, the grid, the Start, Stop, etc buttons, the ComboBox for pre-made patterns
 
 	private JFrame frame;
 	private JLabel label;
 	private JPanel panel;
 	private JButton[] button;
+	private JComboBox patterns;
 	
 	public MainGui() {
 		frame = new JFrame();
@@ -26,17 +16,30 @@ public class MainGui {
 		frame.setSize(1000,1000);
 		frame.setLocationRelativeTo(null);
 		
+		panel = new JPanel();
+		
 		label = new JLabel("Hi");
+		
 		button = new JButton[100];
 		
-		panel = new JPanel();
-		panel.add(label);
+		String[] pat = {"Single Cell", "Glider", "Methuselah", "Pentadecathlon","B-Heptomino", "Boat Stretcher", "Switch Engine", "Glider Gun"};
+		patterns = new JComboBox(pat);
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		panel.add(label);
 		frame.setContentPane(panel);
 		frame.setVisible(true);
 	
 		
 	}
 }
-
 
