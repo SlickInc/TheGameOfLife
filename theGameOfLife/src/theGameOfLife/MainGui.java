efb85e281c7960c15106ead16a28616006060663
DeGuzmanJ <DeGuzmanJ@WCMNLABS018LT.blads.ca>
2021-10-26 13:53:32 -0500

    Made frame

--- theGameOfLife/src/theGameOfLife/MainGui.java
+++ theGameOfLife/src/theGameOfLife/MainGui.java
@@ -1,5 +1,22 @@
 package theGameOfLife;
-
+import javax.swing.*;
 public class MainGui {
 	//This will house the main frame, the grid, the Start, Stop, etc buttons, the ComboBox for pre-made patterns
+
+	private JFrame frame;
+	private JLabel label;
+	private JPanel panel;
+	private JButton[] button;
+	
+	public MainGui() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setSize(1000,1000);
+		frame.setLocationRelativeTo(null);
+		
+		label = new JLabel("");
+		button = new JButton[100];
+		
+	}
 }
+
