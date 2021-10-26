fcaf796efae00e414dbafff69fddc2db992212d6
Ahmad <ahmadaldali6021@gmail.com>
2021-10-26 13:56:39 -0500

    added frame

--- theGameOfLife/src/theGameOfLife/MainGui.java
+++ theGameOfLife/src/theGameOfLife/MainGui.java
@@ -1,5 +1,50 @@
 package theGameOfLife;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.color.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class MainGui {
 	//This will house the main frame, the grid, the Start, Stop, etc buttons, the ComboBox for pre-made patterns
+	
+	JFrame frame;
+	JLabel label;
+	JPanel panel;
+	JButton button;
+	
+	public MainGui() {
+		
+		frame = new JFrame("Photo Album");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1200, 700);
+		frame.setLocationRelativeTo(null);
+		
+		panel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		panel.setBackground(Color.BLACK);
+		
+		label = new JLabel();
+		label.setForeground(Color.RED);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		frame.setContentPane(panel);
+
+		frame.setVisible(true);
+	}
+	
 }
