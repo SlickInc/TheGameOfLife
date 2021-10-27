0537b96f5d57f7f14790b46e8975beebafd88597
DeGuzmanJ <DeGuzmanJ@WCMNLABS018LT.blads.ca>
2021-10-27 13:28:57 -0500

    Somewhat of a button grid

--- theGameOfLife/src/theGameOfLife/MainGui.java
+++ theGameOfLife/src/theGameOfLife/MainGui.java
@@ -1,4 +1,9 @@
 package theGameOfLife;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridBagLayout;
+
 import javax.swing.*;
 public class MainGui {
 	//This will house the main frame, the grid, the Start, Stop, etc buttons, the ComboBox for pre-made patterns
@@ -14,8 +19,40 @@ public class MainGui {
 		frame.setSize(1000,1000);
 		frame.setLocationRelativeTo(null);
 		
+		int x = 0;
+		int y = 0;
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		
 		label = new JLabel("Hi");
 		button = new JButton[100];
+		
+		
+		for (int i = 0; i<100; i++) {
+			button[i] = new JButton("");
+			
+			button[i].addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+				
+					
+				}
+			});
+				
+			c.gridx = x;
+			c.gridy= y;
+			panel.add(button[i],c);
+			x++;
+			if(x == 100) {
+				x= 0;
+				y++;		
+			}
+		}
+	
+		
+		
 		
 		panel = new JPanel();
 		panel.add(label);
